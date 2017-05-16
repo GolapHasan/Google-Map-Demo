79e323277b640d290b140314ec2927a0740dde5a
Md. Kamrul Hasan <kamrul@pathao.com>
2017-05-16 15:37:32 +0600

    initial commit

---
+++ GoogleMap Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GoogleMap Demo
+//
+//  Created by Md. Kamrul Hasan on 5/16/17.
+//  Copyright © 2017 MKHG Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func clickMeTapped(_ sender: Any) {
+        print("clickMeTapped")
+    }
+
+}
+
